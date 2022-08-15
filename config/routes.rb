0e3810dd246fff2ee3f8ef9894551e4fae5685ab
Thiago Fabricio <thiago.fabricio123@gmail.com>
2022-08-15 11:38:27 -0300

    Criação da HomePage
    Criação da HomePage da atividade

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
   resources :medicos
+  resources :pacientes
+  resources :homepage
 
-  root "pacientes#index"
+  root "homepage#index"
+
+
   resources :pacientes do
     resources :consults
   end
 
-  resources :medicos
+  resources :medicos do
+    resources :consults
+  end
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
