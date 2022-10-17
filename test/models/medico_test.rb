6b1a3c458f9b923c34eb1457c1743ed3dd9c4f83
Thiago Fabricio <thiago.fabricio123@gmail.com>
2022-10-17 01:30:39 -0300

    Criacao dos Teste de Model de Medico

--- test/models/medico_test.rb
+++ test/models/medico_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
 
 class MedicoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "salvar medico sem nome" do
+    medico = Medico.new
+    medico.nome = ""
+    medico.crm = "12345678901"
+    medico.especialidade = "Cardiologista"
+    medico.email = "jao@jao"
+    medico.cpf = "12345678901"
+
+    assert_not medico.save
+
+  end
+
+  test "salvar medico sem crm" do
+    medico = Medico.new
+    medico.nome = "João da Silva"
+    medico.crm = ""
+    medico.especialidade = "Cardiologista"
+    medico.email = "jao@jao"
+    medico.cpf = "12345678901"
+
+    assert_not medico.save
+
+  end
+
 end
