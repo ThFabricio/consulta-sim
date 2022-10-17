a46a735a3c651dff5b934b2b66618e1fc35fe438
Thiago Fabricio <thiago.fabricio123@gmail.com>
2022-10-17 01:35:01 -0300

    Criacao dos teste de consulta

--- test/models/consult_test.rb
+++ test/models/consult_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class ConsultTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "criar consulta sem paciente" do
+    consult = Consult.new
+    consult.medico_id = 1
+    consult.data = "2018-10-10"
+    consult.horario = "10:00"
+    assert_not consult.save, "Salvou consulta sem paciente"
+  end
+
+  test "criar consulta sem medico" do
+    consult = Consult.new
+    consult.paciente_id = 1
+    consult.data = "2018-10-10"
+    consult.horario = "10:00"
+    assert_not consult.save, "Salvou consulta sem medico"
+  end
 end
