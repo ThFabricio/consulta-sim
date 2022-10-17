42021bedf1147b318b52d11adb4e45895a9510d4
Thiago Fabricio <thiago.fabricio123@gmail.com>
2022-10-17 11:28:57 -0300

    Correcoes no steps de Paciente

--- features/step_definitions/paciente_steps.rb
+++ features/step_definitions/paciente_steps.rb
@@ -23,33 +23,41 @@ Then('o paciente deve ser cadastrado com sucesso') do
   expect(page).to have_content 'Paciente was successfully created.'
 end
 
-Given('eu tenha um paciente cadastrado com cpf {string}') do |string|
+Given('exite um paciente que tem nome {string}, data {string}, cpf {string}, email {string}, logradouro {string}, bairro {string}, cidade {string}, complemento {string} e cep {string}') do |string, string2, string3, string4, string5, string6, string7, string8, string9|
   visit '/pacientes/new'
   expect(page).to have_content('New paciente')
 
-  fill_in 'paciente[cpf]', with: string
-  fill_in 'paciente[nome]', with: 'João'
-  fill_in 'paciente[data]', with: '01/01/2000'
-  fill_in 'paciente[email]', with: 'joaodojao@email'
-  fill_in 'paciente[endereco_attributes][logradoro]', with: 'Rua'
-  fill_in 'paciente[endereco_attributes][bairro]', with: 'Bairro'
-  fill_in 'paciente[endereco_attributes][cidade]', with: 'Cidade'
-  fill_in 'paciente[endereco_attributes][complemento]', with: 'Complemento'
-  fill_in 'paciente[endereco_attributes][cep]', with: '12345678'
+  fill_in 'paciente[nome]', with: string
+  fill_in 'paciente[data]', with: string2
+  fill_in 'paciente[cpf]', with: string3
+  fill_in 'paciente[email]', with: string4
+  fill_in 'paciente[endereco_attributes][logradoro]', with: string5
+  fill_in 'paciente[endereco_attributes][bairro]', with: string6
+  fill_in 'paciente[endereco_attributes][cidade]', with: string7
+  fill_in 'paciente[endereco_attributes][complemento]', with: string8
+  fill_in 'paciente[endereco_attributes][cep]', with: string9
   click_button 'Create Paciente'
-  expect(page).to have_content (string)
+  expect(page).to have_content(string)
 end
 
-When('eu clico no paciente com cpf {string}') do |string|
+Given('eu esteja na tela de paciente') do
+  visit '/pacientes'
+  expect(page).to have_content('Pacientes')
+end
+
+Given('eu visualizo o paciente de cpf {string}') do |string|
   id = Paciente.where(cpf: string).first.id
   find("a[href='/pacientes/#{id}']").click
   expect(page).to have_content(string)
 end
 
-When('eu clico em deletar o paciente') do
-  click_link 'Destroy this paciente'
+When('eu clicar em deletar') do
+  click_button 'Destroy this paciente'
 end
 
 Then('o paciente deve ser deletado com sucesso') do
   expect(page).to have_content 'Paciente was successfully destroyed.'
 end
+
+
+
